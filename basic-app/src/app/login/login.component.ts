c12239e6cf927b6cf971221aa4dd1994a01aee78
Bilel S <bilel.saghrouchni@gmail.com>
2020-11-05 19:55:33 +0100

    login and signup TS ok

--- basic-app/src/app/login/login.component.ts
+++ basic-app/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {AuthService} from "../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,12 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  field_non_valid : boolean = false;
+  bad_mail_password : boolean = false;
+
+  constructor(private httpClient: HttpClient, private authService: AuthService, private router: Router
+              ) { }
 
   ngOnInit(): void {
   }
 
-  onSubmit(f){
-    alert('Not built yet');
+  onSignIn(mail: string, password: string) {
+    /*Envoie du mail et mdp au backend et réception du token de l'user correspondant*/
+    this.httpClient
+      .post(this.authService.backend + 'api/user/login', {mail: mail, password: password})
+      .subscribe(
+        (response) => {
+          this.authService.setUserInfo( JSON.stringify(response['token']), 'token'); //stocke le token dans le session/localStorage
+          this.authService.setUserInfo( JSON.stringify(response['user']), 'user');
+          this.router.navigate(['']);
+        },
+        (error) => {
+          if (error['status'] === 401 || error['status'] === 400) {
+            this.bad_mail_password = true;
+          }
+        }
+      );
+  }
+
+  onSubmit(form: NgForm) {
+    if (this.testMail(form.value['mail'])){
+      this.field_non_valid = false;
+      this.onSignIn(form.value.mail, form.value.password); // PASSPORT
+    }else{
+      this.field_non_valid = true;
+      form.reset();
+    }
+
+  }
+
+  testMail(mail:string){
+    for(let elt of mail){
+      if(elt === '@'){
+        return true;
+      }
+    }
+    return false;
   }
 }
--- basic-app/src/app/signup/signup.component.ts
+++ basic-app/src/app/signup/signup.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-signup',
@@ -23,17 +24,37 @@ export class SignupComponent implements OnInit {
   same_password = true;
   info_mdp = false;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, private router: Router, private auth: AuthService) { }
 
   ngOnInit(): void {
   }
 
-  onSubmit(f){
-    alert("Not build yet");
-  }
+  onSubmit(form: NgForm) {
+    if (this.checkSamePassword(form)) {
+      this.same_password = true;
+      const name = form.value.lastName;
+      const prenom = form.value.firstName;
+      const sexe = form.value.sex;
+      const mail = form.value.mail;
+      const password = form.value.password;
+      this.addUser(name, prenom, sexe, mail, password);
 
-  updateSurname(text: string) {
-    this.current_surname = text;
+      // this.auth.isAuth = true;
+    } else if (form.value.password.length < 8) {
+      form.reset();
+      this.strong_password = false;
+      this.medium_password = false;
+      this.bad_password = false;
+      this.same_password = true;
+      alert('Le mot de passe doit faire au moins 6 caractères.');
+    } else {
+      form.reset();
+      this.strong_password = false;
+      this.medium_password = false;
+      this.bad_password = false;
+      this.same_password = true;
+      alert('Les mots de passe entrés ne sont pas identiques.');
+    }
   }
 
   checkSamePassword(form: NgForm) {
@@ -72,4 +93,31 @@ export class SignupComponent implements OnInit {
     }
 
   }
+
+  addUser(nom: string, prenom: string, sexe: string, mail: string, password: string, ){
+    const newUser = {firstName: '', lastName: '', sex: '', mail:'', password:''};
+    newUser.firstName = prenom;
+    newUser.lastName = nom;
+    newUser.sex = sexe;
+    newUser.mail = mail;
+    newUser.password = password;
+
+    this.httpClient
+      .post(this.auth.backend + 'api/user', newUser)
+      .subscribe(
+        (response) => {
+          //Reception of the token which is set in the localStorage
+          this.alreadyExist = false;
+          this.auth.setUserInfo( JSON.stringify(response['token']), 'token'); //stocke le token dans le session/localStorage
+          this.auth.setUserInfo( JSON.stringify(response['user']), 'user');
+          this.router.navigate(['']);
+        },
+        (error) => {
+          if(error['status'] === 400){
+            this.alreadyExist = true;
+          }
+        }
+      );
+  }
+
 }
